f70bfc42dae48da6bf0dddbc15b15e929cebbf06
Justine <103948035+Justine3237@users.noreply.github.com>
2024-04-12 20:56:14 +0530

    Add files via upload

---
+++ Install.sh
@@ -0,0 +1,20 @@
+g="\033[1;32m"
+r="\033[1;31m"
+b="\033[1;34m"
+w="\033[0m"
+red='\e[1;31m'
+default='\e[0m'
+yellow='\e[0;33m'
+orange='\e[38;5;166m'
+green='\033[92m'
+
+clear
+sleep 1.5
+echo -e "$default"
+pkg update
+pkg upgrade
+pkg install git
+pkg install python
+cd
+clear
+cd
